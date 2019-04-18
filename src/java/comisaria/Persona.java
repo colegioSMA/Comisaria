a36d911db3f646100270d05959045fa19019eeeb
jruiz <jose.ruiz@colegiosma.com>
2019-04-19 00:01:06 +0200

    cambios

--- src/java/comisaria/Persona.java
+++ src/java/comisaria/Persona.java
@@ -21,4 +21,11 @@ public class Persona {
 		return direccion.
 				contains("calle");
 	}
+	public void setIdentificador(int id) {
+		if (id < 0)
+			throw new IllegalArgumentException();
+		else 
+			this.identificador = id;
+	}
+
 }
