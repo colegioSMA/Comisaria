504eccea1f5d638ea3c428917ee64e0208c9c3fb
ManuelCorral <pietrodeocre@android>
2019-04-19 14:06:40 +0200

    Añadido Ejercicio 6

---
+++ src/test/addDetenidoYPoliciaTest.java
@@ -0,0 +1,71 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import comisaria.Comisaria;
+import comisaria.Persona;
+import comisaria.Policia;
+
+public class addDetenidoYPoliciaTest {
+	
+	Policia[] policia = null;
+	Persona[] persona = null;
+	
+	Comisaria comisaria = null;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		policia = new Policia[3];
+		persona = new Persona[3];
+		
+		comisaria = new Comisaria(policia, persona);
+		
+		
+		
+		
+	}
+
+	@Test
+	public void addDetenidoTest() {
+		
+		Persona[] persona2 = new Persona[6];
+		
+		persona2[0] = null;
+		persona2[1] = null;
+		persona2[2] = null;
+		persona2[3] = new Persona("Manuel", 19, "calle Direccion1");
+		persona2[4] = new Persona("Pedro", 16, "calle Direccion2");
+		persona2[5] = new Persona("Miguel", 35, "calle Direccion3");
+		
+		comisaria.addDetenido(persona2[3]);
+		comisaria.addDetenido(persona2[4]);
+		comisaria.addDetenido(persona2[5]);
+		
+		assertArrayEquals(persona2, comisaria.getPersonasDetenidas());
+		
+	}
+	
+public void addPoliciaTest() {
+		
+		Policia[] policia2 = new Policia[6];
+		
+		policia2[0] = null;
+		policia2[1] = null;
+		policia2[2] = null;
+		policia2[3] = new Policia("Manuel", "calle Direccion1");
+		policia2[4] = new Policia("Pedro", "calle Direccion2");
+		policia2[5] = new Policia("Miguel", "calle Direccion3");
+		
+		comisaria.addPolicia(policia2[3]);
+		comisaria.addPolicia(policia2[4]);
+		comisaria.addPolicia(policia2[5]);
+		
+		assertArrayEquals(policia2, comisaria.getPolicias());
+		
+	}
+
+}
