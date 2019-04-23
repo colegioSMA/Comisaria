fddf1f09bfd0c309165919c24776e4072762c26e
Candido Esperanza <candiespem@gmail.com>
2019-04-23 23:03:27 +0200

    Pruebas unitarias Finales

--- src/java/Test/ComisariaTest.java
+++ src/java/Test/ComisariaTest.java
@@ -11,51 +11,49 @@ import comisaria.*;
 
 public class ComisariaTest {
 
-	Policia[] policias=null;
-	Persona[] personasDetenidas=null;
-	Comisaria comisaria = null;	
-	
+	Policia[] policias = null;
+	Persona[] personasDetenidas = null;
+	Comisaria comisaria = null;
+	Policia policia1 = null;
+	Policia policia2 = null;
+	Persona detenido1 = null;
+	Persona detenido2 = null;
 
 	@Before
 	public void setUp() throws Exception {
-		
-		Policia[] policias = {new Policia("Juan", "c/Alva")};
-		Persona[] personasDetenidas = {new Persona("Candi",19,"c/vitrubio")};
-		comisaria = new Comisaria(policias, personasDetenidas);	
-		
-		
+
+		policia2 = new Policia("Juan", "c/Alva");
+		policia1 = new Policia("Jose", "c/Segovia");
+		detenido1 = new Persona("Jose", 22, "c/Segovia");
+		detenido2 = new Persona("Pedro", 45, "c/Alvatroz");
+
+		Policia[] policias = { policia1 };
+		Persona[] personasDetenidas = { detenido1 };
+		comisaria = new Comisaria(policias, personasDetenidas);
 	}
 
 	@Test
-	public void addPoliciaTest() {	
-		
-		Policia policia2=new Policia("Jose","c/Segovia");
-		
-		Policia[] policiasAux= {new Policia("Juan", "c/Alva"),new Policia("Jose","c/Segovia")};		
-		
+	public void addPoliciaTest() {
+
+		Policia[] policiasAux = { policia1, policia2 };
+
 		comisaria.addPolicia(policia2);
-		
-		assertNotNull(comisaria.getPolicias()[comisaria.getPolicias().length-1]);
-		assertArrayEquals(policiasAux,comisaria.getPolicias());
+
+		assertNotNull(comisaria.getPolicias()[comisaria.getPolicias().length - 1]);
+		assertArrayEquals(policiasAux, comisaria.getPolicias());
 
 	}
-	
+
 	@Test
 	public void addDetenidoTest() {
-		
-		Persona detenido = new Persona("Jose",22,"c/Segovia");
-		
-		Persona[] detenidosAux= {new Persona("Candi",19,"c/vitrubio"),new Persona("Jose",22,"c/Segovia")};	
-		
-		comisaria.addDetenido(detenido);
-		
-		assertNotNull(comisaria.getPersonasDetenidas()[comisaria.getPersonasDetenidas().length-1]);
-		
+
+		Persona[] detenidosAux = { detenido1, detenido2 };
+
+		comisaria.addDetenido(detenido2);
+
+		assertNotNull(comisaria.getPersonasDetenidas()[comisaria.getPersonasDetenidas().length - 1]);
 		assertArrayEquals(detenidosAux, comisaria.getPersonasDetenidas());
 
 	}
-	
-	
-
 
 }
