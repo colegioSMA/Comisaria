3ce3ec93e6261633f026147a36392c4bc7dc9f15
fabriciogalvez <fabriciogabe@gmail.com>
2019-04-24 18:50:39 +0200

    No he realizado cambios

---
+++ src/java/comisaria/TestComisaria.java
@@ -0,0 +1,34 @@
+package comisaria;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author OUR
+ *
+ */
+public class TestComisaria {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
